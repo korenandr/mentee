9da95a18cd36b445187b289c55362a2641e8d9fe
Poison1488 <96549459+Poison1488@users.noreply.github.com>
2022-02-04 21:47:54 +0300

    задание_3.py
    как тебе такое Илон Маск

--- task1/залание_3.py
+++ task1/залание_3.py
@@ -1,3 +1,35 @@
-l = {}
-l = {1,2,3,4, {'s','t','r','o','k','a'}}
-print(l)
+from random import randint
+random_chislo = randint(0,10)
+user_variable_1 = input("Сейчас у вас будет 3 попытки отгадать число.Введите любое число от 1 до 10-")
+if user_variable_1.isdigit():
+    if int(user_variable_1) == random_chislo:
+        print("Вы отгадали,я загадала число-",random_chislo)
+    else:
+        if random_chislo < int(user_variable_1):
+            print("Я загадала число меньше чем ваше")
+        else:
+            print("Я загадала число больше чем ваше")
+else:
+    print("Вы попытались меня обмануть!")
+user_variable_2 = input("Вторая попытка-")
+if user_variable_2.isdigit():
+    if int(user_variable_2) == random_chislo:
+        print("Вы отгадали,я загадала число-",random_chislo)
+    else:
+        if random_chislo < int(user_variable_2):
+            print("Я загадала число меньше чем ваше")
+        else:
+            print("Я загадала число больше чем ваше")
+else:
+    print("Вы попытались меня обмануть!")
+user_variable_3 = input("Третья попытка-")
+if user_variable_3.isdigit():
+    if int(user_variable_3) == random_chislo:
+        print("Вы отгадали,я загадала число-",random_chislo)
+    else:
+        if random_chislo < int(user_variable_3):
+            print("Я загадала число меньше чем ваше-",random_chislo)
+        else:
+            print("Я загадала число больше чем ваше-",random_chislo)
+else:
+    print("Вы попытались меня обмануть!")
