4e93310bc3afb8edf3a79d585cb97d83850700d1
Poison1488 <96549459+Poison1488@users.noreply.github.com>
2022-02-04 12:19:09 +0300

    задание_2.py
    исправил недочеты

--- task1/задание_2.py
+++ task1/задание_2.py
@@ -1,26 +1,26 @@
-variable_1 = input("Введите любое число-")
-if variable_1.isdigit():
+user_variable = input("Введите любое число-")
+if user_variable.isdigit():
     print("Сейчас вам нужно будет ввести код операции:1-для работы с четными числами,2-для работы с нечетными числами")
-    operashion_cod = input("Введите код операции-")
-    if operashion_cod.isdigit():
-        if int(operashion_cod) == 1:
-            if int(variable_1) % 2 != 0:
+    operation_code = input("Введите код операции-")
+    if operation_code.isdigit():
+        if int(operation_code) == 1:
+            if int(user_variable) % 2 != 0:
                 print("Вы ввели не четное число,попробуйте ввести код операции 2")
             else:
-                variable_3 = int(variable_1)
-                while int(variable_3) >= 0:
-                    print(variable_3,end = '.')
-                    variable_3 -= 2
+                while_variable_1 = int(user_variable)
+                while while_variable_1 >= 0:
+                    print(while_variable_1,end = '.')
+                    while_variable_1 -= 2
         else:
-            if operashion_cod.isdigit():
-                if int(operashion_cod) == 2:
-                    if int(variable_1) % 2 == 0:
+            if operation_code.isdigit():
+                if int(operation_code) == 2:
+                    if int(user_variable) % 2 == 0:
                         print("Вы ввели не четное число,попробуйте ввести код операции 1")
                     else:
-                        variable_4 = int(variable_1)
-                        while int(variable_4) >= 0:
-                            print(variable_4, end = '.')
-                            variable_4 -= 2                           
+                        while_variable_2 = int(user_variable)
+                        while while_variable_2 >= 0:
+                            print(while_variable_2, end = '.')
+                            while_variable_2 -= 2                           
     else:
         print("Не пытайтесь снова меня обмануть!")        
 else:
