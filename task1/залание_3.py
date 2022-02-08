570bf6b84bd8642cc8fac8d04be243d8cc23e8d3
Poison1488 <96549459+Poison1488@users.noreply.github.com>
2022-02-08 16:52:13 +0300

    задание_3.py
    все исправил

--- task1/залание_3.py
+++ task1/залание_3.py
@@ -1,35 +1,19 @@
+print("Сыграем в игру,я загадала число от 1 до 10,а у вас будет 3 попытки чтобы его отгадать,погнали...")
 from random import randint
-random_chislo = randint(0,10)
-user_variable_1 = input("Сейчас у вас будет 3 попытки отгадать число.Введите любое число от 1 до 10-")
-if user_variable_1.isdigit():
-    if int(user_variable_1) == random_chislo:
-        print("Вы отгадали,я загадала число-",random_chislo)
-    else:
-        if random_chislo < int(user_variable_1):
-            print("Я загадала число меньше чем ваше")
+random_number = randint(1,10)
+counter = 3
+while counter >= 1:
+    counter -= 1
+    user_variable = input("Введите ваше число-")
+    if user_variable.isdigit():
+        if int(user_variable) == random_number:
+            print("Вы отгадали,я загадала число-",random_number)
+            break
         else:
-            print("Я загадала число больше чем ваше")
-else:
-    print("Вы попытались меня обмануть!")
-user_variable_2 = input("Вторая попытка-")
-if user_variable_2.isdigit():
-    if int(user_variable_2) == random_chislo:
-        print("Вы отгадали,я загадала число-",random_chislo)
+            if int(user_variable) < random_number:
+                print("Я загадал число больше чем ваше")
+            else:
+                print("Я загадала число меньше чем ваше")
     else:
-        if random_chislo < int(user_variable_2):
-            print("Я загадала число меньше чем ваше")
-        else:
-            print("Я загадала число больше чем ваше")
-else:
-    print("Вы попытались меня обмануть!")
-user_variable_3 = input("Третья попытка-")
-if user_variable_3.isdigit():
-    if int(user_variable_3) == random_chislo:
-        print("Вы отгадали,я загадала число-",random_chislo)
-    else:
-        if random_chislo < int(user_variable_3):
-            print("Я загадала число меньше чем ваше-",random_chislo)
-        else:
-            print("Я загадала число больше чем ваше-",random_chislo)
-else:
-    print("Вы попытались меня обмануть!")
+        print("Вы ввели не верный тип данных,попробуйте снова.")
+        break
