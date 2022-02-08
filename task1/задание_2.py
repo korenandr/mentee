b9098602566451ca17161a509d6ff96bd44b7fef
Poison1488 <96549459+Poison1488@users.noreply.github.com>
2022-02-08 20:45:06 +0300

    задание_2.py
    так-то лучше

--- task1/задание_2.py
+++ task1/задание_2.py
@@ -1,26 +1,25 @@
-user_variable = input("Введите любое число-")
-if user_variable.isdigit():
-    print("Сейчас вам нужно будет ввести код операции:1-для работы с четными числами,2-для работы с нечетными числами")
-    operation_code = input("Введите код операции-")
-    if operation_code.isdigit():
-        if int(operation_code) == 1:
-            if int(user_variable) % 2 != 0:
-                print("Вы ввели не четное число,попробуйте ввести код операции 2")
-            else:
-                counter= int(user_variable)
-                while counter >= 0:
-                    print(counter,end = '.')
-                    counter -= 2
-        else:
-            if int(operation_code) == 2:
-                if int(user_variable) % 2 == 0:
-                    print("Вы ввели не четное число,попробуйте ввести код операции 1")
-                else:
-                    counter = int(user_variable) 
-                    while counter >= 0:
-                        print(counter, end = '.')
-                        counter -= 2                           
+print("сейчас вам нужно будет ввести любое число и код операции. 1-для работы с нечетными числами.2-для работы с четными числами.")
+counter_1 = 1
+while counter_1 >= 1:
+    counter_1 -= 1
+    try:
+        user_variable = int(input("Введите любое число-"))
+        operation_code = int(input("Введите код операци-"))
+    except:
+        print("Вы ввели не верные параметры")
+        break
+    if operation_code == 1:
+        if user_variable % 2 != 0:
+            counter = user_variable
+            while counter >= 0:
+                print(counter,end = '.')
+                counter -= 2
     else:
-        print("Не пытайтесь снова меня обмануть!")        
-else:
-    print("Вы должны ввести число и оно не должно быть отрицательным!")
+        if operation_code == 2:
+            if user_variable % 2 == 0:
+                counter = user_variable
+                while counter >= 0:
+                    print(counter, end = '.')
+                    counter -= 2 
+        else:
+            print("Вы ввели не верный код операции")                      
