66b28b24c6bca0f8f6ec44a64a399d1077349676
Poison1488 <96549459+Poison1488@users.noreply.github.com>
2022-02-19 18:07:25 +0300

    задание_2.py
    исправил

--- task1/задание_2.py
+++ task1/задание_2.py
@@ -1,13 +1,10 @@
 print("сейчас вам нужно будет ввести любое число и код операции. 1-для работы с нечетными числами.2-для работы с четными числами.")
-counter_1 = 1
-while counter_1 >= 1:
-    counter_1 -= 1
-    try:
-        user_variable = int(input("Введите любое число-"))
-        operation_code = int(input("Введите код операци-"))
-    except:
-        print("Вы ввели не верные параметры")
-        break
+try:
+    user_variable = int(input("Введите любое число-"))
+    operation_code = int(input("Введите код операци-"))
+except:
+    print("Вы ввели не верные параметры")
+else:
     if operation_code == 1:
         if user_variable % 2 != 0:
             counter = user_variable
@@ -16,10 +13,9 @@ while counter_1 >= 1:
                 counter -= 2
     else:
         if operation_code == 2:
-            if user_variable % 2 == 0:
-                counter = user_variable
-                while counter >= 0:
-                    print(counter, end = '.')
-                    counter -= 2 
+            counter = user_variable
+            while counter >= 0:
+                print(counter, end = '.')
+                counter -= 2 
         else:
-            print("Вы ввели не верный код операции")                      
+            print("Вы ввели не верный код операции")
