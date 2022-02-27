eb648e59b6f5476ebbe190e5adb4bd3caa0c8d3b
Poison1488 <96549459+Poison1488@users.noreply.github.com>
2022-02-27 17:08:36 +0300

    задание_4.py
    исправил недочеты

--- task1/задание_4.py
+++ task1/задание_4.py
@@ -1,16 +1,18 @@
 try:
     list = list(map(int,input("Введите список элементов:").split()))
     number = int(input("Введите число:"))
-    operations_cod = int(input("Введите код операции:"))
+    operation_cod = int(input("Введите код операции:"))
 except:
     print("Вы ввели не верный тип данных")
 else:
-    if operations_cod == 1:
+    if operation_cod == 1:
         for tmp in list:
             if number < tmp:
                 print (tmp,end = ' ')
     else:
-        if operations_cod == 0:
+        if operation_cod == 0:
             for tmp in list:
                 if number > tmp:
                     print (tmp,end = ' ')
+        else:
+            print("Вы ввели не верный тип данных")
