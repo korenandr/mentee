f5bb05b77d99bfac34b2ee501998d1407b2eb3e0
Poison1488 <96549459+Poison1488@users.noreply.github.com>
2022-02-27 17:08:10 +0300

    задание_2.py
    так лучше? Или все таки лучше было другим способом сделать,я мог сделать с помощью циклов и списков,но подумал о том,что когда ты мне давно давал это задание,я о списках еще не знал

--- task1/задание_2.py
+++ task1/задание_2.py
@@ -1,21 +1,27 @@
 print("сейчас вам нужно будет ввести любое число и код операции. 1-для работы с нечетными числами.2-для работы с четными числами.")
 try:
-    user_variable = int(input("Введите любое число-"))
-    operation_code = int(input("Введите код операци-"))
+    min_range = int(input("Введите минимальный диапазон:"))
+    max_range = int(input("Введите максимальный диапазон:"))
+    operation_code = int(input("Введите код операци:"))
 except:
     print("Вы ввели не верные параметры")
 else:
     if operation_code == 1:
-        if user_variable % 2 != 0:
-            counter = user_variable
-            while counter >= 0:
-                print(counter,end = '.')
+        if max_range % 2 != 0:
+            counter = max_range
+            while counter >= min_range:
+                print(counter,end = ' ')
                 counter -= 2
+        else:
+            print("Вы ввели не верный максимальный диапазон для данного кода операции")
     else:
         if operation_code == 2:
-            counter = user_variable
-            while counter >= 0:
-                print(counter, end = '.')
-                counter -= 2 
+            if max_range % 2 == 0:
+                counter = max_range
+                while counter >= min_range:
+                    print(counter, end = ' ')
+                    counter -= 2 
+            else:
+                print("Вы ввели не верный максимальный диапазон для данного кода операции")
         else:
             print("Вы ввели не верный код операции")
