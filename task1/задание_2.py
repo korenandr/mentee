c8c4f376b7c50435ad416dbc54affe5ea8212515
Poison1488 <96549459+Poison1488@users.noreply.github.com>
2022-03-20 13:54:09 +0300

    задание_2.py
    все исправил

--- task1/задание_2.py
+++ task1/задание_2.py
@@ -5,13 +5,11 @@ try:
 except:
     print("Вы ввели не верные параметры")
 else:
-    if operation_code == 0:
+    if operation_code <= 1 and operation_code >= 0:
         for tmp in range(min_range,max_range):
-            if tmp % 2 == 0:
+            if operation_code == 0 and tmp % 2 == 0:
                 print(tmp,end = ' ')
-    elif operation_code == 1:
-        for tmp in range(min_range,max_range):
-            if tmp % 2 != 0:
+            elif operation_code == 1 and tmp % 2 != 0:
                 print(tmp,end = ' ')
     else:
-        print("Вы ввели не верный код операции")
+        print('Вы ввели не верный код операции')
